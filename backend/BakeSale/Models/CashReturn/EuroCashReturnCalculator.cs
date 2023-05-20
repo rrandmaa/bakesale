ccd311ff94502557bdeb5a80562903694fbc24e2
Rainer Randmaa <rairan@ttu.ee>
2023-05-20 19:17:36 +0300

    Reformatted euroNotesAndCoins const

--- backend/BakeSale/Models/CashReturn/EuroCashReturnCalculator.cs
+++ backend/BakeSale/Models/CashReturn/EuroCashReturnCalculator.cs
@@ -3,22 +3,22 @@
     public static class EuroCashReturnCalculator
     {
         private static readonly decimal[] euroNotesAndCoins =
-            {
-                200m,
-                100m,
-                50m,
-                20m,
-                10m,
-                5m,
-                2m,
-                1m,
-                0.5m,
-                0.2m,
-                0.1m,
-                0.05m,
-                0.02m,
-                0.01m
-            };
+        {
+            200m,
+            100m,
+            50m,
+            20m,
+            10m,
+            5m,
+            2m,
+            1m,
+            0.5m,
+            0.2m,
+            0.1m,
+            0.05m,
+            0.02m,
+            0.01m
+        };
 
         public static List<CashReturnResponseLine> FindReturnCurrencyNotes(decimal inputAmount)
         {
