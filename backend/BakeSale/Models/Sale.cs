7c5efdb8ea3aa54b016bfaa914ae98583d2e96fb
Rainer Randmaa <rairan@ttu.ee>
2023-05-24 16:24:42 +0300

    ValidatePurchase method for Sale model

--- backend/BakeSale/Models/Sale.cs
+++ backend/BakeSale/Models/Sale.cs
@@ -8,5 +8,22 @@ namespace BakeSale.Models
         public string? Name { get; set; }
         public SaleStatus Status { get; set; }
         public IEnumerable<Product> Products { get; set; } = new List<Product>();
+        public bool ValidatePurchase(Purchase purchase)
+        {
+            foreach (PurchaseLine purchaseLine in purchase.PurchaseLines)
+            {
+                Product? product = Products.FirstOrDefault(x => x.Id == purchaseLine.ProductId);
+
+                if (product is null) { 
+                    return false; 
+                }
+
+                if (purchaseLine.Quantity > product.RemainingQuantity)
+                {
+                    return false;
+                }
+            }
+            return true;
+        } 
     }
 }
