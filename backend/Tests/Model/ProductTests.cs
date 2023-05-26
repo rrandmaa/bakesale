0a51bc8b465d1c5539d276a5d1ce29b4ceba7d36
Rainer Randmaa <rairan@ttu.ee>
2023-05-26 23:28:36 +0300

    Unit tests for remainingQuantity

---
+++ backend/Tests/Model/ProductTests.cs
@@ -0,0 +1,25 @@
+﻿using BakeSale.Models;
+
+namespace Tests.Model
+{
+    [TestClass]
+    public class ProductTests
+    {
+        [DataRow(10, 4, 2)]
+        [DataRow(10, 5, 0)]
+        [DataRow(10, 0, 10)]
+        [TestMethod] public void RemainingQuantityTest(int initalQuanity, int singlePurchaseLineQuantity, int expected)
+        {
+            var product = TestDataHelper.NewProduct(TestDataHelper.GetId());
+            product.InitialQuantity = initalQuanity;
+
+            product.PurchasesLines = new List<PurchaseLine>()
+            {
+                TestDataHelper.NewPurchaseLine(TestDataHelper.GetId(), product.Id, singlePurchaseLineQuantity),
+                TestDataHelper.NewPurchaseLine(TestDataHelper.GetId(), product.Id, singlePurchaseLineQuantity),
+            };
+
+            Assert.AreEqual(expected, product.RemainingQuantity);
+        }
+    }
+}
